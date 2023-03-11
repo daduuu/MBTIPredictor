3863ab98dc9cc07538b9e5b6a6b23f6ebc403a4f
David Du <daviddu0425@gmail.com>
2023-03-11 03:19:49 -0800

    fixed name bugs

--- T5_training_file.py
+++ T5_training_file.py
@@ -9,7 +9,7 @@ import wandb
 import pickle
 
 dict = {
-    "model_name": model_name,
+    "model_name": model_t5,
     "learning_rate": learning_rate,
     "max_length_input": max_length_input,
     "doTruncate": doTruncate,
@@ -29,15 +29,15 @@ wandb.init(
 
 device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelWithLMHead.from_pretrained(model_name).to(device)
+tokenizer = AutoTokenizer.from_pretrained(model_t5)
+model = AutoModelWithLMHead.from_pretrained(model_t5).to(device)
 model.train()
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
 training_dataset_input = load_from_disk("dataset/train_dataset_input")
-training_dataset_labels = load_from_disk("Dataset/train_dataset_labels")
+training_dataset_labels = load_from_disk("dataset/train_dataset_labels")
 validation_dataset_input = load_from_disk("dataset/validation_dataset_input")
-validation_dataset_labels = load_from_disk("Dataset/validation_dataset_labels")
+validation_dataset_labels = load_from_disk("dataset/validation_dataset_labels")
 
 training_dataset = Dataset.from_dict({"input_ids": training_dataset_input['input_ids'], #train input ids
                              "labels_input_ids": training_dataset_labels['input_ids'] #label input ids
@@ -54,29 +54,41 @@ val_dataloader = DataLoader(validation_dataset, batch_size=batch_size, drop_last
 step = -1
 
 for epoch in range(epochs):
-    for i, batch in tqdm(enumerate(train_dataloader)):
+    train_acc = 0
+    train_loss = 0
+    step = 0
+    for batch in tqdm(train_dataloader):
         step += 1
         optimizer.zero_grad()
         loss = model(input_ids=batch["input_ids"],
                      labels=batch["labels_input_ids"]).loss
+        train_loss += loss.item()
 
         wandb.log({"train loss":loss.item(),
-                   "batch_index": i,
+                   "batch_index": step,
                    "epoch": epoch,
                    "step": step})
         loss.backward()
         optimizer.step()
 
-        if i % 200 == 0:
-            model.eval()
-            val_loss = 0
-            count = 0
-            for j, val_batch in enumerate(val_dataloader):
-                loss = model(input_ids=val_batch["input_ids"],
-                             labels=val_batch["labels_input_ids"]).loss # compute loss
-                val_loss += loss.item()
-                count += 1
+    
+    model.eval()
+    total_acc_val = 0
+    total_loss_val = 0
+    count = 0
 
-            wandb.log({"validation loss": val_loss / count,
-                       "step": step})
-            model.train()
+    for val_batch in val_dataloader:
+        loss = model(input_ids=val_batch["input_ids"],
+                        labels=val_batch["labels_input_ids"]).loss # compute loss
+        total_loss_val += loss.item()
+        count += 1
+
+    print(f'Epoch: {epoch} | Train Loss: {train_loss / len(training_dataset): .5f} \
+                | Train Accuracy: {train_acc / len(training_dataset): .5f} \
+                | Val Loss: {total_loss_val / len(validation_dataset): .5f}\
+                | Val Accuracy: {total_acc_val / len(validation_dataset): .5f}')
+    wandb.log({"validation loss":total_loss_val / len(validation_dataset),
+                "validation accuracy":total_acc_val / len(validation_dataset),
+                    "train accuracy":train_acc / len(training_dataset),
+                "step": step})
+    model.train()
